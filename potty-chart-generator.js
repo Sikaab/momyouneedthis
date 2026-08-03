cf8d4963249e326775d13eb0dd72de3e0a28aee8
Sikaab <99841815+Sikaab@users.noreply.github.com>
2026-08-03 13:55:23 -0400

    Update potty-chart-generator.js

--- potty-chart-generator.js
+++ potty-chart-generator.js
@@ -960,7 +960,7 @@ pdf.setFontSize(28);
 pdf.text(
 themes[chartData.theme].couponTitle,
 148,
-25,
+5,
 {
 align:"center"
 }
@@ -981,7 +981,7 @@ pdf.setFontSize(14);
 pdf.text(
 themes[chartData.theme].couponText,
 148,
-38,
+10,
 {
 align:"center",
 maxWidth:220
